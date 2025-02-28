75b39d53ae77038b6a9a335db3136a6a46b1b4ee
SuperDuperBlade <catmasterking@gmail.com>
2025-02-28 22:51:23 +0000

    updated help cmd

--- Main.cpp
+++ Main.cpp
@@ -19,7 +19,7 @@ int main(int argc,char* argv[]) {
 	parser.addOption(mangaID_identifier, "The id of the manga you want to download from mangaDex", true, true);
 	parser.addOption(outputDir_identifier, "The directory you want to write the manga to (defualt is the dir the script is running in)", false, true);
 	parser.addOption("-m", "The mode you want to download the manga in can be either volumes , chapters or manga.", false, true);
-	parser.addOption("-dt", "The quality of the downloaded images , lower quality will take up less space. options: data , datasaver", false , true);
+	parser.addOption("-dt", "The quality of the downloaded images , lower quality will take up less space. options: data , datasaver, both", false , true);
 	parser.passArguments(argc, argv);
 	
 	MangaDex dex(&parser,logger);
